929ae616a7415114823e407fed32e0070142abc9
Christopher Little <hephistocles@gmail.com>
2014-10-12 00:38:27 +0100

    Limit search results

--- ms/mopidy-server.js
+++ ms/mopidy-server.js
@@ -67,10 +67,28 @@ var MopidyQueue = function (config) {
 		var words = query.split(" ");
 		if (this.ready) {
 			this.mopidy.library.search({'any': words}).done(function(results) {
-				callback({
-					"success": true,
-					"results": results
-				});
+				var result = {
+					success: true,
+					results: {}
+				};
+				for (var i = 0; i < results.length; i++) {
+					var tracks = [];
+					if (results[i].tracks) {
+						tracks = results[i].tracks.slice(0,10);
+					}
+					switch (results[i].uri.split(":")[0]) {
+						case "soundcloud":
+							result.results.soundcloud = tracks;
+						break;
+						case "local":
+							result.results.local = tracks;
+						break;
+						case "spotify":
+							result.results.spotify = tracks;
+						break;
+					}
+				}
+				callback(result);
 			});
 		} else {
 			callback({
@@ -87,12 +105,12 @@ var MopidyQueue = function (config) {
 					callback({success:false, error: e});
 				})
 				.done(function() {
-					callback({success:true})
+					callback({success:true});
 				});
 		} else {
 			callback({success: false, error: "not ready to search"});
 		}
-	}
+	};
 
 };
 
