c8992ebb304d7c918ba53722c7de4fc8731a3250
SpeedoDevo <speedo@devo.hu>
2014-10-11 23:04:47 +0100

    retain login

--- ca/public/js/Controllers.js
+++ ca/public/js/Controllers.js
@@ -1,54 +1,39 @@
-angular.module('Controllers', []).controller('MainController', function($scope) {
+angular.module('Controllers', []).controller('MainController', function($scope, $location) {
 
-  $scope.tagline = 'To the moon and back!'; 
+  $scope.$on('$routeChangeSuccess', function () {
+    $scope.user = Parse.User.current();
+  });
+
+  $scope.$on('loginSuccess',function(e,user){
+    console.log(user);
+    $scope.user = user;
+    $location.path('/find');
+  });
 
 }).controller('HomeController', function($scope) {
-
-  // $scope.createNewUser = function() {
-  //   var user = new Parse.User();
-  //   user.set("username", $scope.user);
-  //   user.set("password", $scope.password);
-  //   user.set("credit", 9);
-  //   // user.set("email", "daniellowtw@hotmail.com");
-  //   user.signUp(null, {
-  //     success: function(user) {
-  //     },
-  //     error: function(user, error) {
-  //     }
-  //   });
-  // }
-
-  // $scope.userLogin = function() {
-  //   Parse.User.logIn($scope.user, $user.password, {
-  //     success: function(user) {
-  //         // Do stuff after successful login.
-  //     },
-  //     error: function(user, error) {
-  //         // The login failed. Check error to see why.
-  //     }
-  //   });
-  // }
 
   $scope.logReg = function(){
     console.log($scope.username, $scope.password);
     Parse.User.logIn($scope.username, $scope.password, {
       success: function(user){
-        console.log(user);
+        $scope.$emit('loginSuccess',user);
+        $scope.error = false;
+        $scope.$apply();
       },
       error: function(user, error) {
-        console.log(user);
-        console.log(error);
         var nUser = new Parse.User();
         nUser.set("username", $scope.username);
         nUser.set("password", $scope.password);
         nUser.set("credit", 10);
         nUser.signUp(null, {
           success: function(user){
-            console.log(user);
+            $scope.$emit('loginSuccess',user);
+            $scope.error = false;
+            $scope.$apply();
           },
           error: function(user, error){
-            console.log(user);
-            console.log(error);
+            $scope.error = true;
+            $scope.$apply();
           }
         })
       }
