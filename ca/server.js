b1676a2fe9bde2f9d7c32e1e73ba51f6c7345a1d
Christopher Little <hephistocles@gmail.com>
2014-10-11 10:45:28 -0400

    Modifications to ca server.js

--- ca/server.js
+++ ca/server.js
@@ -1,1 +1,9 @@
-console.log("Hello world!");
+var http = require('http');
+
+http.createServer(function (req, res) {
+	console.log("Received a request");	
+	res.writeHead(200, {"Content-Type": "text/plain"});
+	res.end("Hello world");
+}).listen(3005);
+
+console.log("Server runing");
